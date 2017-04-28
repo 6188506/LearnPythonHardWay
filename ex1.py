64456474f9ada5f52f4166e37b13044fc8213d88
6188506 <6188506@qq.com>
2017-04-28 09:27:19 +0800

    first commit

---
+++ ex1.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+print "hello world hello again I like typing this. This is fun. Yay! Printing I'd much rather you 'not'. I said do not touch this." 
+print " "
+#我认为这是注释
---
+++ ex3.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+#A comment,this is so you can read your program later.
+#Anything after the # is ignored by python.
+
+print u"I could have code like this.这是中国吗？" #and the comment after is ignored 
+# you can also use a comment to "disable" or comment out a piece of code:
+# print "this won't run"
+
+print u"这个很重要呦. "
+
