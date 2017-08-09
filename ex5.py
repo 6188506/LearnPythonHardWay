5f42128b81f48b07237fd3d82b0e5592865bbed1
6188506 <6188506@qq.com>
2017-08-09 17:44:45 +0800

    修改了ex5.py

--- ex5.py
+++ ex5.py
@@ -3,7 +3,7 @@ my_age = 35 # not a lie
 my_height = 74 # inches
 my_weight = 180 # lbs
 my_eyes = 'Blue'
-my_teeth = White
+my_teeth = 'White'
 my_hair = 'Brown'
 
 print "Let's talk about  %s." % my_name
