c3dd5eaae32af9cc187915be9907b41f757e00ef
6188506 <6188506@qq.com>
2017-09-13 08:34:31 +0800

    update in company

--- ex26-1.py
+++ ex26-1.py
@@ -75,15 +75,16 @@ print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_p
 
 sentence = "All god\tthings come to those who weight."
 
-words = ex25.break_words(sentence)
-sorted_words = ex25.sort_words(words)
+words = break_words(sentence)
+sorted_words = sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
 print_first_word(sorted_words)
 print_last_word(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
-print_sorted_words
+
+sorted_words = sort_sentence(sentence)
+print sorted_words
 
 print_first_and_last(sentence)
-print_first_a_last_sorted(sentence)
+print_first_and_last_sorted(sentence)
