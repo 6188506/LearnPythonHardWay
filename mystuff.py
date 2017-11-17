fd6df3563a41fc1e0fc7ebcab82da93e5c4204a8
6188506 <6188506@qq.com>
2017-11-17 08:58:34 +0800

    modify mystuff

--- mystuff.py
+++ mystuff.py
@@ -1,11 +1,18 @@
+# -*- coding: utf-8 -*-
+i = 1000   
+''' 这是我的
+多行注释'''
+
 def apple():
 	print "I AM APPLES!(FROM MODULE)"
 	abc = "living reflection of a dream"	
 	tangerine = "Living reflection of a dream"
-	global x 
+	x = 1000
+	y = 17777
+
 	
  
  
 def tt():
-	pass
+	pass       
  
