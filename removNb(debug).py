214e4975022b3b42fbdf120463cf2aab9268d97c
6188506 <6188506@qq.com>
2018-01-03 10:39:05 +0800

    camel_case

--- removNb(debug).py
+++ removNb(debug).py
@@ -5,3 +5,8 @@ def removNb(n):
 		    if sum(range(1, n+1))-(a+b)==a*b:
 			    result.append((a,b))
     return result
+	
+###################################3
+def removNb(n):
+    
+    return [(a,b) for a in range(1,n+1) for b in range(1,n+1) if sum(range(1,n+1))-(a+b)==a*b]
