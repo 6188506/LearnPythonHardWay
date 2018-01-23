8c08aac71a52a057ef6ea2f065c7f1805cb49ecf
6188506 <6188506@qq.com>
2018-01-23 15:08:22 +0800

    python challenge_2

--- pythonchallenge_1.py
+++ pythonchallenge_1.py
@@ -12,4 +12,14 @@ def convert_alp(text):
 			result.append(i)
 	print ''.join(result)	
 	
+i hope you didnt translate it by hand. thats what computers are for. 
+doing it in by hand is inefficient and that's why this text is so long.
+ using string.maketrans() is recommended. now apply on the url	
 #using string.maketrans() is recommended.(我要查下怎么使用！)	
+
+必须在python3.x下完成
+
+def convert_alp(text):
+	instr = "abcdefghijklmnopqrstuvwxyz"
+	outstr= "cdefghijklmnopqrstuvwxyzab"
+	return text.translate(text.maketrans(instr, outstr))
