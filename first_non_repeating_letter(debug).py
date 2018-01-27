d46a18e649443afdc8c86dc60cbaaa226b960e94
6188506 <6188506@qq.com>
2018-01-27 22:26:44 +0800

    first non repeating letter

--- first_non_repeating_letter(debug).py
+++ first_non_repeating_letter(debug).py
@@ -1,9 +1,9 @@
-def first_non_repeating_letter(stringX):
-    import string
-    for i in stringX:
-        if  65 <= ord(i) <= 90 and  stringX.count(i) == 1 and stringX.count(i.lower()) == 0:
-            return i
-        elif  97 <= ord(i) <= 122 and  stringX.count(i) == 1 and stringX.count(i.upper()) == 0:
-            return i
-        elif stringX.count(i) == 1 and i in string.punctuation:    
-            return i
+def first_non_repeating_letter(string):
+    dict = {}
+    counter = 0
+    for index, value in enumerate(string):
+        dict[index] = value
+    for i in string.lower():
+        counter += 1
+        if string.lower().count(i)==1:
+            return dict[counter-1]
