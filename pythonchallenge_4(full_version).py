859ce6db887a8d401bfe4cbd46476a478c14096d
6188506 <6188506@qq.com>
2018-01-30 15:34:26 +0800

    python challenge_4

---
+++ pythonchallenge_4(full_version).py
@@ -0,0 +1,25 @@
+#-*- coding=utf-8 -*-
+#必须在python 3.x环境下
+
+import urllib.request
+import re
+number = 12345
+
+for i in range(400):
+	num_str = ''
+	url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=%d'%number
+	
+	if i == 85:
+		url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=two'
+	f = urllib.request.urlopen(url)
+	webPage = f.read().decode()                  #=>网页的内容并从bytes转换成string
+	number = int(re.findall('\d{3,8}', webPage)[0])
+	
+	print(webPage)
+	print(i)
+    
+print (number)
+
+
+
+i = 355  得到了结果peak
