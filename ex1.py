78dfa169738b2ec30dfc65d99ad0cfd7e5254fe6
6188506 <6188506@qq.com>
2018-02-09 09:56:52 +0800

    weight for weight

--- ex1.py
+++ ex1.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
-print "hello world hello again I like typing this. This is fun. Yay! Printing I'd much rather you 'not'. I said do not touch this." 
-print " 艹，这么吊"
-#我认为这是注释
+print "hello world hello again I like typing this. This is fun. Yay! Printing I'd much rather you 'not'. I said do not touch this."
+print " 艹锟斤拷锟斤拷么锟斤拷"
 
+
+def water_test():
+    print "oh,this is a good test"
+
+
+water_test()
+
+print 'ok I just wanna test debug function'
---
+++ order_weight(string).py
@@ -0,0 +1,5 @@
+def order_weight(string):
+    str_spl = string.split()
+    digit_sum = [sum(map(int,list(i)))for i in str_spl]
+    bind_sort = sorted(zip(digit_sum, str_spl))
+    return ' '.join(zip(*bind_sort)[1])
